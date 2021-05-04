f79f6b127a0e1a105c3fde1f6c4a48b2ea9e5794
esraakocak <esra1kocak@gmail.com>
2021-05-04 23:54:50 +0300

    odeccommit

---
+++ coffeshop/src/Concrete/CustomerCheckManager.java
@@ -0,0 +1,12 @@
+package Concrete;
+
+import Abstract.CustomerCheckService;
+
+import Entities.Customer;
+
+public class CustomerCheckManager implements CustomerCheckService{
+
+	public boolean CheckPerson(Customer customer) {
+		return true;
+	}
+}
