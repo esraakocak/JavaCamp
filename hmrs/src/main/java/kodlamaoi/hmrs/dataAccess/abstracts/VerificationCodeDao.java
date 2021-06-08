0a7aa9bf03c34059412eb074c6bfae00a4cdc1fb
esra koçak <56760393+esraakocak@users.noreply.github.com>
2021-06-08 22:17:29 +0300

    Delete hmrs directory

--- hmrs/src/main/java/kodlamaoi/hmrs/dataAccess/abstracts/VerificationCodeDao.java
+++
@@ -1,9 +0,0 @@
-package kodlamaoi.hmrs.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlamaoi.hmrs.entities.concretes.VerificationCode;
-
-public interface VerificationCodeDao extends JpaRepository<VerificationCode, Integer>{
-
-}
